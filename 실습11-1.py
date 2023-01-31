23f1551fd9f429f6b0fb9fe64a187b4ab6a85f5b
김현준 <112222072+Hyun3246@users.noreply.github.com>
2023-01-31 17:22:42 +0900

    first commit

---
+++ 실습11-1.py
@@ -0,0 +1,21 @@
+import random
+
+def play_the_game():
+    user_input = int(input("원하는 숫자를 입력하세요: "))
+    answer = random.randint(1, user_input)
+    while True:
+        guess = int(input("예상 숫자를 입력하세요(0은 종료): "))
+        if guess < answer:
+            print("숫자가 너무 작습니다. 다시 입력하세요.")
+            continue
+        elif guess > answer:
+            print("숫자가 너무 큽니다. 다시 입력하세요.")
+            continue
+        elif guess == 0:
+            print('종료합니다.')
+            break
+        else:
+            print("정답입니다!")
+            break
+
+play_the_game()
