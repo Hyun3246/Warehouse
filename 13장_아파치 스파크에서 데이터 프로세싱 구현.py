937acd9693b5c03fe12dce141a66d163a7182879
김현준 <112222072+Hyun3246@users.noreply.github.com>
2023-02-06 16:27:55 +0900

    first commit

---
+++ 13장_아파치 스파크에서 데이터 프로세싱 구현.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('cloudanum').getOrCreate()
+
+df = spark.read.csv('taxi2.csv', inferSchema=True, header=True)
+
+print(df.columns)
+
+df.createOrReplaceTempView("main")
+
+data = spark.sql("SELECT payment_type, Count(*) AS COUNT, AVG(fare_amount), AVG(tip_amount) AS AverageFare from main GROUP BY payment_type")
+
+data.show()
