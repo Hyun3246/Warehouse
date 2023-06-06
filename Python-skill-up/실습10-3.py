b41d83783209c05136f7927eac401da15f0ff31d
HyunJun <guswns6370@gmail.com>
2023-06-06 13:41:44 +0900

    실습예제 설명 추가

--- Python-skill-up/실습10-3.py
+++ Python-skill-up/실습10-3.py
@@ -1,3 +1,5 @@
+# Fraction 클래스를 이용한 분수 덧셈 계산기
+
 from fractions import Fraction
 
 total = Fraction('0')
@@ -6,7 +8,7 @@ while True:
     s = input('Enter fraction (press ENTER to quit): ')
     if ',' in s:
         s = s.replace(',', '/')
-    s = s.replace(' ', '')
+    s = s.replace(' ', '')      # 사용자가 빈칸을 입력하는 경우 방지
     if not s:
         break
     total += Fraction(s)
