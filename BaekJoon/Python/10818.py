fbec7674324665aa6da9f9b95a3f6d3026c3a9a6
HyunJun <guswns6370@gmail.com>
2023-12-13 22:56:24 +0900

    Create 10818.py

---
+++ BaekJoon/Python/10818.py
@@ -0,0 +1,13 @@
+N = int(input())
+num_list = list(map(int, input().split()))
+
+max = -1000001
+min = 1000001
+
+for i in num_list:
+    if max < i:
+        max = i
+    if min > i:
+        min = i
+
+print(min, max)
