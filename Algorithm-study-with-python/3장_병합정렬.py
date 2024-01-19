210eb2830c89f87f309ae0c6a839ec3467c9ff17
HyunJun <guswns6370@gmail.com>
2024-01-19 15:26:04 +0900

    코드 개선 및 설명 추가

--- Algorithm-study-with-python/3장_병합정렬.py
+++ Algorithm-study-with-python/3장_병합정렬.py
@@ -1,18 +1,18 @@
 def MergeSort(list):
     if len(list) > 1:
-        # 리스트를 반으로 나눕니다.
+        # 리스트를 반으로 나눈다.
         mid = len(list) // 2
-        left = list[:mid]
-        right = list[mid:]
+        left = list[:mid]   # 앞 반쪽
+        right = list[mid:]  # 뒤 반쪽
 
-        # 나뉜 부분의 크기가 1이 될 때까지 반복합니다.
+        # 나뉜 부분의 크기가 1이 될 때까지 반복해서 쪼갠다.
         MergeSort(left)
         MergeSort(right)
 
-        a = 0
-        b = 0
-        c = 0
+        a, b = 0, 0  # 병합(merge) 과정 종료 여부를 판단하기 위한 장치
+        c = 0   # 정렬된 리스트 인덱스
 
+        # 병합(merge) 과정
         while a < len(left) and b < len(right):
             if left[a] < right[b]:
                 list[c] = left[a]
@@ -22,6 +22,7 @@ def MergeSort(list):
                 b = b + 1
             c = c + 1
         
+        # left, right 어느 한 쪽에 남은 요소들을 처리해보자.
         while a < len(left):
             list[c] = left[a]
             a = a + 1
