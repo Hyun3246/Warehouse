820733c1e8ffb2bdbc47da276223f2e33ea07d7e
HyunJun <guswns6370@gmail.com>
2024-01-19 15:16:49 +0900

    코드 개선 및 설명 추가

--- Algorithm-study-with-python/3장_삽입정렬.py
+++ Algorithm-study-with-python/3장_삽입정렬.py
@@ -1,9 +1,11 @@
 def InsertionSort(list):
     for i in range(1, len(list)):
+        # key 선정
         j = i - 1
-        element_next = list[i]
-        while (list[j] > element_next) and (j >= 0):
+        key = list[i]
+
+        while (list[j] > key) and (j >= 0):
             list[j + 1] = list[j]
             j = j - 1
-        list[j + 1] = element_next
+        list[j + 1] = key
     return list
