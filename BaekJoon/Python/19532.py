733ea45a9c1e8e4573a066f0d366562d81e46174
HyunJun <guswns6370@gmail.com>
2024-04-01 22:52:40 +0900

    Create 19532.py

---
+++ BaekJoon/Python/19532.py
@@ -0,0 +1,9 @@
+import sys
+
+a, b, c, d, e, f = map(int, sys.stdin.readline().strip().split())
+
+for x in range(-999, 999 + 1):
+    for y in range(-999, 999 + 1):
+        if (a * x + b * y == c) and (d * x + e * y == f):
+            print(x, y)
+            break
