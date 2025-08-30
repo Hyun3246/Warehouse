1ac92e49f2090f9a3243d94a9695ef8bae9cdf2f
HJ <guswns6370@gmail.com>
2025-08-30 12:05:05 +0900

    Add min-heap implementation in 1927.java
    Implement min-heap functionality to process input values.

---
+++ BaekJoon/Java/1927.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) {
+                // 배열에서 가장 작은 값을 출력하고 그 값을 제거하는 로직
+                if (minHeap.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(minHeap.poll());
+                }
+            } else {
+                // 배열에 x라는 값을 넣는 로직
+                minHeap.add(x);
+            }
+        }
+    }
+}
